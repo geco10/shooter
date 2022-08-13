b7b6ced38ee6096f2988eabebcf230e1526f8a1b
Dilena Koplande <koplande@yahoo.com>
2022-08-13 13:35:47 -0500

    added more code.

--- sfml.cpp
+++ sfml.cpp
@@ -3,6 +3,7 @@
 using namespace sf;
 class Target:public Drawable {
 private:
+	Vector2f speed = {6,10};
 	Vector2f loc;
 	float radius;
 	int score;
@@ -15,13 +16,39 @@ private:
 	int calc() {
 		return 80 - radius;
 	}
-	void draw(RenderTarget &target,RenderStates states)const override {
+	void move() {
+		loc.y = loc.y + speed.y;
+		loc.x = loc.x + speed.x;
+	}
+	void draw(RenderTarget& target, RenderStates states)const override {
 		CircleShape circle(radius);
-		circle.setFillColor(Color(255,155,0));
-		circle.setOutlineThickness(radius / 10);
-		circle.setOutlineColor(Color(98,0,255));
+		circle.setFillColor(Color(255,255,255));
+		circle.setOutlineThickness(radius / 2);
+		circle.setOutlineColor(Color(255,0,0));
 		circle.setPosition(Vector2f(loc));
 		target.draw(circle);
+	}
+};
+class TargetCenter :public Drawable{
+	std::vector<Target> arr;
+	Vector2f screen;
+	int max=7;
+public:
+	TargetCenter(Vector2f screen) {
+		this->screen=screen;
+	}
+	void move() {
+		for (int i = 0; i < arr.size(); ++i) {
+			arr[i].move();
+		}
+	}
+	bool make(int radius,Vector2f loc) {
+		arr.push_back(Target(radius,loc));
+	}
+	void draw(RenderTarget& target, RenderStates states)const override {
+		for (int i = 0; i < arr.size(); ++i) {
+			target.draw(arr[i]);
+		}
 	}
 };
 int main()
@@ -29,7 +56,7 @@ int main()
 
 	// Объект, который, собственно, является главным окном приложения
 	RenderWindow window(VideoMode(500, 300), "SFML Works!");
-
+	Target target(16, Vector2f(30, 30));
 	// Главный цикл приложения: выполняется, пока открыто окно
 	while (window.isOpen())
 	{
@@ -41,8 +68,10 @@ int main()
 			if (event.type == Event::Closed)
 				window.close(); // тогда закрываем ег
 		}
-		window.clear(Color(255,255,255));
-		Target target(12,Vector2f(150,200));
+
+		window.clear(Color(0,255,0));
+		target.move();
+		sleep((milliseconds(30)));
 		window.draw(target);
 		window.display();
 	}
