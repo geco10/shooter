69206e176c231b5873ca24c2c33e906ccd251332
Dilena Koplande <koplande@yahoo.com>
2022-08-28 20:36:58 -0500

    adeeddddddd morrrre ccccooooodddeee.

--- sfml.cpp
+++ sfml.cpp
@@ -1,13 +1,13 @@
 ﻿#include <SFML/Graphics.hpp>	
 
 using namespace sf;
-class Target:public Drawable {
+class Target :public Drawable {
 private:
-	Vector2f speed = {6,10};
+	Vector2f speed = { 6,10 };
 	Vector2f loc;
 	float radius;
 	int score;
-	public:
+public:
 	Target(float radius, Vector2f loc) {
 		this->radius = radius;
 		this->loc = loc;
@@ -20,43 +20,84 @@ private:
 		loc.y = loc.y + speed.y;
 		loc.x = loc.x + speed.x;
 	}
+	bool ifShot() {
+		int y_m = 0;
+		int x_m = 0;
+		if (y_m == loc.y && x_m == loc.x) {
+			return true;
+		}
+		else return false;
+	}
 	void draw(RenderTarget& target, RenderStates states)const override {
 		CircleShape circle(radius);
-		circle.setFillColor(Color(255,255,255));
+		circle.setFillColor(Color(255, 255, 255));
 		circle.setOutlineThickness(radius / 2);
-		circle.setOutlineColor(Color(255,0,0));
+		circle.setOutlineColor(Color(255, 0, 0));
 		circle.setPosition(Vector2f(loc));
 		target.draw(circle);
 	}
+	int locY() {
+		return loc.y;
+	}
 };
-class TargetCenter :public Drawable{
+class TargetCenter :public Drawable {
 	std::vector<Target> arr;
 	Vector2f screen;
-	int max=7;
+	int max = 7;
 public:
 	TargetCenter(Vector2f screen) {
-		this->screen=screen;
+		this->screen = screen;
+	}
+	void trySpamn() {
+		if (arr.size() < max) {
+			int radius = rand() % 50 + 20;
+			Vector2f loc;
+			loc.x = rand() % (int)screen.x;
+			loc.y = radius * (-2);
+			make(radius, loc);
+		}
 	}
 	void move() {
 		for (int i = 0; i < arr.size(); ++i) {
 			arr[i].move();
 		}
 	}
-	bool make(int radius,Vector2f loc) {
-		arr.push_back(Target(radius,loc));
+	void make(int radius, Vector2f loc) {
+		arr.push_back(Target(radius, loc));
+	}
+	int shotCheck() {
+		int Tscore;
+		for (int i = 0; i < arr.size(); ++i) {
+			if (arr[i].ifShot() == true) {
+				Tscore = arr[i].calc();
+				arr.erase(arr.begin() + i);
+		    }
+		}
+		return Tscore;
 	}
 	void draw(RenderTarget& target, RenderStates states)const override {
 		for (int i = 0; i < arr.size(); ++i) {
 			target.draw(arr[i]);
 		}
 	}
+	void clean() {
+		for (int i = 0; i < arr.size(); i++) {
+			if (screen.y < arr[i].locY()) arr.erase(arr.begin() + i);
+		}
+	}
+	void tick() {
+		move();
+		trySpamn();
+		clean();
+	}
 };
 int main()
 {
-
+	srand(time(NULL));
+	Vector2f size(925, 700);
+	TargetCenter tc(size);
 	// Объект, который, собственно, является главным окном приложения
-	RenderWindow window(VideoMode(500, 300), "SFML Works!");
-	Target target(16, Vector2f(30, 30));
+	RenderWindow window(VideoMode(size.x, size.y), "SFML Works!");
 	// Главный цикл приложения: выполняется, пока открыто окно
 	while (window.isOpen())
 	{
@@ -67,12 +108,15 @@ int main()
 			// Пользователь нажал на «крестик» и хочет закрыть окно?
 			if (event.type == Event::Closed)
 				window.close(); // тогда закрываем ег
+			if (event.type == Event::MouseButtonPressed) {
+				//	event.mouseButton.
+			}
 		}
 
-		window.clear(Color(0,255,0));
-		target.move();
+		window.clear(Color(0, 255, 0));
+		tc.tick();
 		sleep((milliseconds(30)));
-		window.draw(target);
+		window.draw(tc);
 		window.display();
 	}
 
